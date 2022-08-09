1a0a3f6c2a788aa342d36c97177bd6815237bcd7
roodhouse <rughjm@gmail.com>
2022-08-08 19:47:56 -0500

    cw

--- weekThree/04-Stu_Hello-Variable/Unsolved/script.js
+++ weekThree/04-Stu_Hello-Variable/Unsolved/script.js
@@ -1,1 +1,19 @@
 // Write Your JavaScript Code Here
+
+var personName = 'John';
+var pets       = 2;
+var funFact    = 'I have 4 kids, all girls.';
+
+console.log('My name is ' + personName + '.');
+console.log('I have ' + pets + ' dogs.');
+console.log('Fun fact: ' + funFact + '..');
+
+personName = 'Brett';
+pets       = 4;
+funFact    = 'Likes to wear red.';
+
+console.log('My name is ' + personName + '.');
+console.log('I have ' + pets + ' dogs.');
+console.log('Fun fact: ' + funFact + '..');
+
+console.log(personName + '+' + pets + '+' + funFact);
