2ae07e89d5dc7390bb6c68cc29cc0f31ccb281b0
roodhouse <rughjm@gmail.com>
2022-08-15 18:22:21 -0500

    wk 4 day 1

---
+++ weekFour/02-Stu_Window-Object/Unsolved/script.js
@@ -0,0 +1,13 @@
+// TODO: What will the following line of code log?
+console.log(window);
+
+// TODO: What will the following line of code log?
+console.log(window.document);
+
+// TODO: What will the following line of code log?
+console.log(document.documentElement);
+
+// TODO: What will the following line of code log?
+console.log(document.head);
+
+!THIS IS A TEST!!!2345s
