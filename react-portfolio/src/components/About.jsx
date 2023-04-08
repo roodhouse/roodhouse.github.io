8c18775bd5f8770549783c4a3f4acd6347548ef7
roodhouse <rughjm@gmail.com>
2023-04-07 22:12:11 -0500

    Skills and Work

---
+++ react-portfolio/src/components/About.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div name='about' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+        <div className='flex flex-col justify-center items-center w-full h-full'>
+            <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
+                <div className='sm:text-right pb-8 pl-4'>
+                    <p className='text-4xl font-bold inline border-b-4 border-[#FF5757]'>About</p>
+                </div>
+                <div></div>
+            </div>
+                <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
+                    <div className='sm:text-right text-4xl font-bold'>
+                        <p>Hi. I'm John, nice to meet you. Please take a look around.</p>
+                    </div>
+                    <div>
+                        <p>I'm a web developer with a background in small business operations including product development, managerial skills and customer service. Earned a certificate from the University of Texas web development program. Entrepreneur who created, developed and managed new products for Rugh Design. Excited to find ways to utilize a self starter attitude in a new industry.</p>
+                    </div>
+                </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
--- react-portfolio/src/components/Home.jsx
+++ react-portfolio/src/components/Home.jsx
@@ -3,8 +3,23 @@ import {HiArrowNarrowRight} from 'react-icons/hi'
 
 function Home() {
   return (
-    <div className='w-full h-screen bg-[#0a192f]'>
+    <div name='home' className='w-full h-screen bg-[#0a192f]'>
 
+        {/* container */}
+
+        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+            <p className='text-[#FF5757]'>Hi, my name is</p>
+            <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>John Rugh</h1>
+            <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Web Developer.</h2>
+            <p className='text-[#8892b0] py-4 max-w-[700px]'>I'm a web developer with a background in small business operations including product development, managerial skills and customer service. Earned a certificate from the University of Texas web development program. Entrepreneur who created, developed and managed new products for Rugh Design. Excited to find ways to utilize a self starter attitude in a new industry.</p>
+            <div>
+                <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#FF5757] hover:border-[#FF5757]'>View Work 
+                    <span className='group-hover:rotate-90 duration-300'>
+                    <HiArrowNarrowRight className='ml-3' />
+                    </span>
+                </button>
+            </div>
+        </div>
     </div>
   )
 }
